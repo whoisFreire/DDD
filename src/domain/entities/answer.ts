96d7d4a9fdc3013dcf53faf3fc91e6aa59a1ef2b
Leonardo Freire Russi <freirerussi@gmail.com>
2025-09-12 13:50:48 -0300

    Getters & Setters das entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -15,6 +15,38 @@ export class Answer extends Entity<AnswerProps> {
     return this.props.content
   }
 
+  get questionId() {
+    return this.props.questionId
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.content
+      .substring(0, 120)
+      .trimEnd()
+      .concat('...')
+  }
+
+  touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
   static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityId) {
     const answer = new Answer({
       ...props,
