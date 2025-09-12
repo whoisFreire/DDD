003dc48edb4e2de4ae362ee0efd746fb185ea2a8
Leonardo Freire Russi <freirerussi@gmail.com>
2025-09-12 12:43:15 -0300

    Mapeando propriedades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from "@/core/entities/entity"
+import { UniqueEntityId } from "@/core/entities/unique-entity-id"
 
 interface AnswerProps {
   content: string
-  questionId: string
-  authorId: string
+  questionId: UniqueEntityId
+  authorId: UniqueEntityId
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
+import { UniqueEntityId } from "@/core/entities/unique-entity-id"
 import { Slug } from "./value-objects/slug"
 import { Entity } from "@/core/entities/entity"
 
 interface QuestionProps {
   title: string
   content: string
-  authorId: string
+  authorId: UniqueEntityId
   slug: Slug
+  bestAnswerId?: UniqueEntityId 
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {}
